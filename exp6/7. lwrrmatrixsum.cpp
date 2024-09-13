fd39fa0e3e992170fa9d125682eb7fb684d8be7b
Arjun Borkar <arjunborkar2006@gmail.com>
2024-09-13 14:13:59 +0530

    Update 7. LwrMatrixSum.cpp

--- exp6/7. lwrrmatrixsum.cpp
+++ exp6/7. lwrrmatrixsum.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 int main()
 {
-    int a[3][3] = {1,2,3,4,5,6,7,8,9};
+    int a[3][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
     int sum = 0;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < (i < 3 ? i : 3); j++) {
