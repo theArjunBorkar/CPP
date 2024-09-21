9b7eeb45eae9d0b038be4748acbf2381edc908f1
Arjun Borkar <arjunborkar2006@gmail.com>
2024-09-21 12:03:28 +0530

    Update test3.cpp

--- exp7/test3.cpp
+++ exp7/test3.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int largest(int a[][3], int n)
 {
-    int mx = a[0][0];
+    int max = a[0][0];
     for (int i = 0; i < n; i++)
         for (int j = 0; j < 3; j++)
-            mx = a[i][j];
-    return mx;
+            max = a[i][j];
+    return max;
 }
 int main()
 {
