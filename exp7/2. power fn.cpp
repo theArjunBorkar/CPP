eebd59f10b4abd503bd2046e909269ec0e48dd57
Arjun Borkar <arjunborkar2006@gmail.com>
2024-09-21 12:39:38 +0530

    Create 2. power fn.cpp

---
+++ exp7/2. power fn.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int power(int x, int y)
+{
+    int res = 1;
+    for (int i = 1; i <= y; i++)
+        res *= x;
+    return res;
+}
+int main()
+{
+    int res, x, y;
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Enter its power: ";
+    cin >> y;
+    cout << x << '^' << y << '=' << power(x, y);
+    return 0;
+}
