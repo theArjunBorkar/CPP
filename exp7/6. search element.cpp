0d26699af29477751cba62dc82203810f37cf3a6
Arjun Borkar <arjunborkar2006@gmail.com>
2024-09-24 18:43:57 +0530

    Update 6. search element.cpp

--- exp7/6. search element.cpp
+++ exp7/6. search element.cpp
@@ -1,55 +1,23 @@
 #include <iostream>
 using namespace std;
-int add_digits(int num) {
-    int digit, sum = 0;
-    while (num != 0) {
-        digit = num % 10;
-        sum += digit;
-        num /= 10;
-    }
-    return sum;
-}
-int rev_digits(int num) {
-    int digit, reverse = 0;
-    while (num != 0) {
-        digit = num % 10;
-        reverse = reverse * 10 + digit;
-        num /= 10;
-    }
-    return reverse;
-}
-int count_digits(int num) {
-    int cnt = 0;
-    while (num != 0) {
-        num /= 10;
-        cnt++;
-    }
-    return cnt;
+bool search_element(int arr[], int n, int index) {
+    for (int i = 0; i < n; i++)
+        if (arr[i] = index)
+            return true;
+    return false;
 }
 int main() {
-    int num, choice;
-    cout << "Enter a number: ";
-    cin >> num;
-    cout << "1. Sum of digits" << '\n';
-    cout << "2. Reverse of a number" << '\n';
-    cout << "3. Number of digits" << '\n';
-    cout << "4. Exit" << '\n';
-    cout << "Enter your choice: ";
-    cin >> choice;
-    switch (choice) {
-    case 1:
-        cout << "Sum of digits = " << add_digits(num);
-        break;
-    case 2:
-        cout << "Reversed number = " << rev_digits(num);
-        break;
-    case 3:
-        cout << "Number of digits = " << count_digits(num);
-        break;
-    case 4:
-        return 1;
-    default:
-        cout << "Invalid input";
-    }
+    int n, index;
+    cout << "Enter array size: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cout << "Enter the element you need to find: ";
+    cin >> index;
+    if (search_element(arr, n, index))
+        cout << "Element found";
+    else cout << "Element not found";
     return 0;
 }
