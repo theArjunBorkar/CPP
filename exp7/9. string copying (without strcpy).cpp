ca1d56356f8b916642b890aaf64cb8f25c2d876e
Arjun Borkar <arjunborkar2006@gmail.com>
2024-09-27 13:38:52 +0530

    Create 9. string copying (without strcpy).cpp

---
+++ exp7/9. string copying (without strcpy).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+void str_copy(char str_1[], char str_2[]){
+    for (int i = 0; str_2[i] != '\0'; i++) {
+        int tmp = str_1[i];
+        str_1[i] = str_2[i];
+        str_2[i] = tmp;
+    }
+}
+int main() {
+    char str_1[100], str_2[100];
+    cout << "Enter string one: ";
+    cin.getline(str_1, 100);
+    cout << "Enter string two: ";
+    cin.getline(str_2, 100);
+    str_copy(str_1, str_2);
+    cout << "Copied string: " << str_1;
+    return 0;
+}
