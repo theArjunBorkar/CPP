e36f89e8cefea5fc1510688821a3c7bc3ef61f6b
Arjun Borkar <arjunborkar2006@gmail.com>
2024-09-30 15:40:31 +0530

    Add files via upload

---
+++ exp8/pointers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int m;
+    cout << "Enter m: ";
+    cin >> m;
+    int a[m];
+    for (int i = 0; i < m; i++)
+        cin >> *(a + i);
+    for (int i = 0; i < m; i++)
+        cout << (a + i) << ' ';
+    return 0;
+}
