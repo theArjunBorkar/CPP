c5b6042eb8641e435d923b5e0095f2f2ee6db381
Arjun Borkar <arjunborkar2006@gmail.com>
2024-09-30 15:38:37 +0530

    Add files via upload

---
+++ exp8/3. HDD size.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+struct specs {
+    char CPU[5];
+    int HDD;
+    int puter_num;
+};
+int main() {
+    specs puter[50];
+    for (int i = 0; i < 2; i++) {
+        cout << "Enter computer number: ";
+        cin >> puter[i].puter_num;
+        fflush(stdin);
+        cout << "Enter your CPU: ";
+        cin.getline(puter[i].CPU, 5) ;
+        fflush(stdin);
+        cout << "Enter your HDD size (in GB): ";
+        cin >> puter[i].HDD;
+    }
+    for (int i = 0; i < 2; i++) {
+        if (puter[i].HDD > 8) {
+            cout << "\nComputer number: " << puter[i].puter_num;
+            cout << "\nCPU: " << puter[i].CPU;
+            cout << "\nHDD: " << puter[i].HDD << "gb";
+        }
+    }
+    return 0;
+}
