d9f8399f8192e6cbd605ab52e442f22ec6ff47c3
Arjun Borkar <arjunborkar2006@gmail.com>
2024-10-09 13:54:27 +0530

    Add files via upload

---
+++ exp9/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+class employee {
+public:
+    char name[20];
+    double salary;
+};
+int main() {
+    employee emp;
+    cout << "Enter name: ";
+    cin.getline(emp.name, 20);
+    cout << "Enter salary: ";
+    cin >> emp.salary;
+    cout << emp.name << '\n';
+    cout << emp.salary;
+    return 0;
+}
