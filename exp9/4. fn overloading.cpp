d51e5090cb398779cec35e3e002e87afc49d3634
Arjun Borkar <arjunborkar2006@gmail.com>
2024-10-10 11:23:21 +0530

    Create 4. fn overloading.cpp

---
+++ exp9/4. fn overloading.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class addition {
+    double a;
+    double b;
+    double c;
+public:
+    double sum(double x, double y) {
+        return x + y;
+    }
+    double sum(double x, double y,double z) {
+        return x + y + z;
+    }
+};
+int main() {
+    addition add;
+    cout << add.sum(5, 10) << endl;
+    cout << add.sum(5, 10, 15) << endl;
+    cout << add.sum(0.7f, 0.5f);
+    return 0;
+}
