956b83b12a23e5b913c02746ae52d45640f42a1b
Arjun Borkar <arjunborkar2006@gmail.com>
2024-10-15 15:05:37 +0530

    Create diamond.cpp

---
+++ exp5/diamond.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 5; j >= i; j--)
+            cout << ' ';
+        for (int k = 1; k <= i; k++)
+            cout << '*' << ' ';
+        cout << endl;
+    }
+    for (int i = 4; i >= 1; i--) {
+        for (int j = 5; j >= i; j--)
+            cout << ' ';
+        for (int k = 1; k <= i; k++)
+            cout << '*' << ' ';
+        cout << endl;
+    }
+    return 0;
+}
