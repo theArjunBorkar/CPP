243a5e0d18d61a47ee55c2e06548b00bcb6daa2e
Arjun Borkar <arjunborkar2006@gmail.com>
2024-10-19 12:32:10 +0530

    Update 4. fn overloading.cpp

--- exp9/4. fn overloading.cpp
+++ exp9/4. fn overloading.cpp
@@ -15,7 +15,7 @@ public:
 int main() {
     addition add;
     cout << add.sum(5, 10) << endl;
-   cout << add.sum(5, 10, 15) << endl;
+    cout << add.sum(5, 10, 15) << endl;
     cout << add.sum(0.7f, 0.5f);
     return 0;
 }
