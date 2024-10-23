de5acffd56f9f01dd97184555d8f9e0afcd3893b
Arjun Borkar <arjunborkar2006@gmail.com>
2024-10-23 15:41:14 +0530

    Add files via upload

---
+++ exp10/1. emp constructor.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class employee {
+    int emp_id;
+    char emp_name[10];
+    double emp_sal;
+public:
+    employee() {}
+    employee(int id, char name[10], double sal) {
+        emp_id = id;
+        for (int i = 0; i < 10; i++)
+            emp_name[i] = name[i];
+        emp_sal = sal;
+    }
+    void display() {
+        cout << "\nEmployee id: " << emp_id;
+        cout << "\nEmployee name: " << emp_name;
+        cout << "\nEmployee salary: " << emp_sal;
+    }
+};
+int main() {
+    employee emp[2];
+    for (int i = 0; i < 2; i++) {
+        int id;
+        char name[10];
+        double sal;
+        cout << "Enter employee id: ";
+        cin >> id;
+        cin.ignore();
+        cout << "Enter employee name: ";
+        cin.getline(name, 10);
+        cin.ignore();
+        cout << "Enter employee salary: ";
+        cin >> sal;
+        cin.ignore();
+        emp[i] = employee(id, name, sal);
+    }
+    for (int i = 0; i < 2; i++) {
+        emp[i].display();
+    }
+    return 0;
+}
