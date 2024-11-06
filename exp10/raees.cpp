3b9be0da0b116025785a169c661f3c9acc942038
Arjun Borkar <arjunborkar2006@gmail.com>
2024-11-06 15:27:59 +0530

    Create raees.cpp

---
+++ exp10/raees.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class dada_dadi {
+public:
+    void zameen() {
+        cout << "Dada-dadi ke paas zameen hai" << endl;
+    }
+};
+class mumma_papa : public dada_dadi {
+public:
+    void bangla() {
+        cout << "Mummy-papa ke paas bangla hai" << endl;
+    }
+};
+class raees_bacchha : public mumma_papa {
+public:
+    void ifone() {
+        cout << "Bacchhe ke paas iPhone 16 Pro Max hai" << endl;
+    }
+};
+int main() {
+    raees_bacchha raees;
+    raees.zameen();
+    raees.bangla();
+    raees.ifone();
+    return 0;
+}
